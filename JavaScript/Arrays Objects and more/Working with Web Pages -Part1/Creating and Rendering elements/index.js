69365626d9fde501909a5193b675a56765d91fd6
yesha0309 <yesha.sonara@gmail.com>
2022-12-29 16:38:55 -0500

    new folder

---
+++ JavaScript/Arrays Objects and more/Working with Web Pages -Part1/Creating and Rendering elements/index.js
@@ -0,0 +1,42 @@
+const createPost = function ({ title, content }) {
+    const socialCard = document.createElement('div');
+    //createElement -- creates html elements like, h1, div ....
+    socialCard.setAttribute('class', 'social-card');
+  
+    const icon = document.createElement('div');
+    icon.setAttribute('class', 'post-icon');
+  
+    const postTitle = document.createElement('div');
+    postTitle.setAttribute('class', 'post-title');
+  
+    const postUser = document.createElement('div');
+    postUser.setAttribute('class', 'post-user');
+  
+    const postContent = document.createElement('div');
+    postContent.setAttribute('class', 'post-content');
+  
+    socialCard.append(icon, postTitle, postUser, postContent);
+  
+    // Adding Content
+    postTitle.innerText = title;
+    postUser.innerText = 'Joe Mockery';
+    postContent.innerText = content;
+  
+    document.body.prepend(socialCard);
+  };
+  
+  createPost({
+    title: 'Midnight Sun',
+    content: `Oh man, last month I was in Sweden and I witnessed the midnight sun. It was a surreal experience to see sunlight at 2 in the morning! Couldn't sleep at all, but felt fresh!`,
+  });
+  
+  createPost({
+    title: 'Working with JavaScript',
+    content: `It's been almost 10 years since I started working with JavaScript and it's awesome to see it grow to a mature and loved language. Kudos JS!`,
+  });
+  
+  createPost({
+    title: 'Selling my Honda Civic',
+    content: `Guys, I'm selling my old Honda Civic automatic. Anyone interested, please call me/DM me`,
+  });
+  
