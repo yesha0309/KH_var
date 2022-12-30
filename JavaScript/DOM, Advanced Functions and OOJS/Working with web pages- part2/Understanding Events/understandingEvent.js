bc78f2e911c3bf1bbb93d27d8443635a80304206
yesha0309 <yesha.sonara@gmail.com>
2022-12-29 22:41:38 -0500

    main

--- JavaScript/DOM, Advanced Functions and OOJS/Working with web pages- part2/Understanding Events/understandingEvent.js
+++ JavaScript/DOM, Advanced Functions and OOJS/Working with web pages- part2/Understanding Events/understandingEvent.js
@@ -11,3 +11,24 @@
  * 
  * To create event listners in js, we use 'addEventListener()'
  */
+
+/*
+EVENT BUBBLING -
+
+    An event that starts at an element bubbles up the hierarchy until it reaches the top most
+        html element.
+    Eg: a button is clicked inside a div, body inside an html file
+        - that button click. looks for event-listner, once found computes the function
+            and triggers an event
+        - user clicks, a click event is observed, If an event listener is registerd, it will
+            intercept and invoke the function attached. And look for its parent class
+        - agter that, the parent element is invoked and tired to find an event handler.
+        - this finding event handler continues till the top most html tag is known as event bubbling
+
+*/
+
+/* EVENT CAPTURING:
+ *  While event bubbling occurs from bottom to top, event capturing occurs top to bottom
+ * 
+ * 
+ */
