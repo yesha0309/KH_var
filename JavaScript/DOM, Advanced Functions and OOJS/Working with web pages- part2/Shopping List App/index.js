b335822c1cc9dec80af9e48da49ec43ea9f34e4d
yesha0309 <yesha.sonara@gmail.com>
2022-12-30 18:11:20 -0500

    shopping app

---
+++ JavaScript/DOM, Advanced Functions and OOJS/Working with web pages- part2/Shopping List App/index.js
@@ -0,0 +1,66 @@
+import {
+  addToShoppingList,
+  addToCompletedList,
+  setPriority,
+  removeItem,
+  clearCompleted,
+} from "./model";
+import { renderShoppingList, renderCompletedList } from "./view";
+
+const itemInput = document.querySelector("input[name='itemInput']");
+const shoppingListDiv = document.querySelector(".shopping-list");
+const completedDiv = document.querySelector(".completed");
+const clearCompletedBtn = document.querySelector("#clear-completed");
+
+itemInput.addEventListener("keyup", function (evt) {
+  if (evt.key === "Enter") {
+    addToShoppingList(evt.target.value);
+    renderShoppingList();
+    this.value = "";
+  }
+});
+
+shoppingListDiv.addEventListener("click", function (evt) {
+  if (evt.target.parentElement.classList.contains("priority-control")) {
+    const priority = evt.target.classList.value;
+    const itemId =
+      evt.target.parentElement.parentElement.getAttribute("data-id");
+    console.log(itemId, priority);//------------------------
+    setPriority(itemId, priority);
+    renderShoppingList();
+  }
+
+  if (evt.target.classList.contains("remove-btn")) {
+    const itemId = evt.target.parentElement.getAttribute("data-id");
+    if (removeItem(itemId)) {
+      renderShoppingList();
+    }
+  }
+});
+
+shoppingListDiv.addEventListener("dragstart", function (evt) {
+  if (evt.target.classList.contains("item")) {
+    const getId = evt.target.getAttribute("data-id");
+    evt.dataTransfer.setData("text/plain", getId);
+  }
+});
+
+completedDiv.addEventListener("drop", function (evt) {
+  const itemId = evt.dataTransfer.getData("text/plain");
+
+  if (itemId) {
+    addToCompletedList(itemId);
+    renderShoppingList();
+    renderCompletedList();
+  }
+});
+
+completedDiv.addEventListener("dragover", function (evt) {
+  evt.preventDefault();
+});
+
+clearCompletedBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  clearCompleted();
+  renderCompletedList();
+});
---
+++ JavaScript/DOM, Advanced Functions and OOJS/Working with web pages- part2/Shopping List App/model.js
@@ -0,0 +1,54 @@
+let shoppingList = [];
+let completedList = [];
+
+export const addToShoppingList = (item) => {
+  const itemId = `${parseInt(
+    Math.random() * 100000000
+  )}-${new Date().getTime()}`;
+
+  shoppingList.push({
+    id: itemId,
+    item,
+    priority: 'normal',
+  });
+};
+
+export const addToCompletedList = (itemId) => {
+  const getItem = shoppingList.find(({ id }) => id === itemId);
+  shoppingList = shoppingList.filter(({ id }) => id !== itemId);
+  completedList = [getItem, ...completedList];
+};
+
+export const setPriority = (itemId, priority) => {
+  shoppingList = shoppingList.map((item) => {
+    if (item.id === itemId) {
+      return {
+        ...item,
+        priority,
+      };
+    }
+
+    return item;
+  });
+};
+
+export const removeItem = (itemId) => {
+  const confirm = window.confirm('Do you really want to delete this item?');
+  if (confirm) {
+    shoppingList = shoppingList.filter(({ id }) => id !== itemId);
+    saveToStore({
+      active: shoppingList,
+      completed: completedList,
+    });
+    return true;
+  }
+
+  return false;
+};
+
+export const clearCompleted = () => {
+  completedList = [];
+};
+
+export const getShoppingList = () => shoppingList;
+export const getCompletedList = () => completedList;
